3a2891f641c1717d9f580b2a1f16393d7bf234e4
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-04-03 15:54:17 -0700

    small changes

--- lua/plugins/override.lua
+++ lua/plugins/override.lua
@@ -1,16 +1,3 @@
-local function new_file_with_name()
-  local filename = vim.fn.input "Enter new file name: "
-  if filename ~= "" then
-    if vim.fn.filereadable(filename) == 1 then
-      print "Error: File already exists."
-    else
-      vim.cmd("edit " .. filename)
-    end
-  else
-    print "No file name was entered."
-  end
-end
-
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
