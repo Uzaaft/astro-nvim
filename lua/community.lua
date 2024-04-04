e55decf6c8c3039ba5f4a6c8eb5c8ded0e435060
Daniel Zapata <danielz@Daniels-MacBook-Pro.local>
2024-04-04 03:04:53 -0700

    added conform.nvim

--- lua/community.lua
+++ lua/community.lua
@@ -2,6 +2,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.project.project-nvim" },
 }
