21bbb23e8bd176387a92d6fcd94cbfaeddfd5ba5
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-06 14:08:30 -0700

    testing out mason tool installer and conform community pack

--- lua/community.lua
+++ lua/community.lua
@@ -2,5 +2,7 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
