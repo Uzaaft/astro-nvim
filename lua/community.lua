553d08d0004b056fcde05a2eca7348eb944f21d8
Daniel Zapata <danielz@Daniels-MBP.attlocal.net>
2024-04-06 11:07:52 -0700

    leave conform alone, stick with none-ls for now

--- lua/community.lua
+++ lua/community.lua
@@ -2,6 +2,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
-  { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.pack.rust" },
 }
