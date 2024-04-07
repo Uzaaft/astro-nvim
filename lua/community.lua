e7129c4b6dd21e14a26c3bc93289bf3d0d3d8698
Uzair Aftab <muaf@nmbu.no>
2024-04-07 10:40:04 +0200

    feat: add lua pack

--- lua/community.lua
+++ lua/community.lua
@@ -4,5 +4,6 @@ return {
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.conform-nvim" },
   { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.utility.mason-tool-installer-nvim" },
 }
