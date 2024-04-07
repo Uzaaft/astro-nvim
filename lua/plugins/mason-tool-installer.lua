f45b0f7c3d3775a8e3cf8b854f8fb5ad04aa82d9
Uzair Aftab <muaf@nmbu.no>
2024-04-07 10:39:59 +0200

    fix: Remove unneeded files

--- lua/plugins/mason-tool-installer.lua
+++
@@ -1,5 +0,0 @@
----@type LazySpec
-return {
-  "WhoIsSethDaniel/mason-tool-installer.nvim",
-  opts = function(_, opts) require("astrocore").list_insert_unique(opts.ensure_installed, { "lua_ls", "stylua" }) end,
-}
